b3aa430d580c3366df8874e8e1fdd2b6cac6c81d
Дмитрий <dimam200312@tut.by>
2021-03-19 17:10:27 +0300

    Separate car from physics

--- src/Model/car.h
+++ src/Model/car.h
@@ -6,13 +6,12 @@
 #include <cmath>
 #include <utility>
 
-#include <QApplication>
-
 #include "src/helpers/vec2f.h"
 #include "wheel.h"
 #include "src/helpers/line.h"
+#include "car_physics.h"
 
-class Car {
+class Car : public CarPhysics {
  public:
   Car(int x,
       int y,
@@ -20,11 +19,7 @@ class Car {
       std::vector<std::vector<std::pair<int, int>>>* borders);
   ~Car() = default;
 
-  void Tick(int time_millisec);
-
-  int GetX() const;
-  int GetY() const;
-  double GetAngle() const;
+  void Tick(int time_millisec) override;
 
   void SetFlagUp(bool flag_up);
   void SetFlagDown(bool flag_down);
@@ -32,40 +27,10 @@ class Car {
   void SetFlagRight(bool flag_right);
 
  private:
-  std::vector<std::vector<std::pair<int, int>>>* borders_ = nullptr;
-  std::vector<Wheel> wheels_{4};
-  Vec2f position_;
-  Vec2f angle_vec_;
-  std::vector<Vec2f> prev_position_list_;
-  std::vector<Vec2f> prev_angle_vec_list_;
-  static constexpr int kSizeOfPreviousPos = 4;
-  Vec2f velocity_;
-  double angular_velocity_ = 0;
-
-  double steering_angle_ = 0;
   bool flag_up_ = false;
   bool flag_down_ = false;
   bool flag_left_ = false;
-
   bool flag_right_ = false;
 
-  double accel_factor = 2.0;
-  double max_speed_forward = 300;
-  double max_speed_backward = 100;
-  double half_front_track_ = 0.75;
-  double max_steering_lock_ = 0.7;
-  double half_rear_track_ = half_front_track_;
-  double half_wheel_base_ = 5.5;
-  double length_ = 18.0;
-  double mass_ = 1000.0;
-  double moment_inertia_ = (mass_ * length_ * length_) / 1.0;
-  double front_coef_friction_ = 100;
-  double rear_coef_friction_ = 80;
-  double max_slip_angle_radians_ = 0.07;
-
-  void ProceedCollisions();
   void ProceedInputFlags();
-  void UpdateWheelsPosAndOrientation();
-  void AdvanceStep(int time_millisec);
-  void CalcAccelerations(Vec2f* accel, double* angular_accel);
 };
