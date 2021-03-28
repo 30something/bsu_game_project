030ac464f411d9dfb91c2312e574b87b318a0856
Дмитрий <dimam200312@tut.by>
2021-03-28 18:21:17 +0300

    I dunno the purpose of this commit

--- src/View/view.cpp
+++ src/View/view.cpp
@@ -8,8 +8,7 @@ View::View(GameController* model, int map_index) :
 
 void View::Repaint(QPainter* painter) {
   // Dis works only for 2 cars
-  double width = painter->window().width();
-  double height = painter->window().height();
+  QSize size(painter->window().width(), painter->window().height());
 
   painter->scale(kScale, kScale);
   std::vector<std::pair<int, int>> coordinates_ = model_->GetCarCoordinates();
@@ -18,41 +17,41 @@ void View::Repaint(QPainter* painter) {
   painter->drawImage(0,
                      0,
                      map_,
-                     coordinates_[0].first - width / 2 / kScale / 2,
-                     coordinates_[0].second - height / kScale / 2,
-                     width / 2 / kScale,
-                     height / kScale);
-  painter->drawImage(width / 2 / kScale,
+                     coordinates_[0].first - size.width() / 2 / kScale / 2,
+                     coordinates_[0].second - size.height() / kScale / 2,
+                     size.width() / 2 / kScale,
+                     size.height() / kScale);
+  painter->drawImage(size.width() / 2 / kScale,
                      0,
                      map_,
-                     coordinates_[1].first - width / 2 / kScale / 2,
-                     coordinates_[1].second - height / kScale / 2,
-                     width / 2 / kScale,
-                     height / kScale);
+                     coordinates_[1].first - size.width() / 2 / kScale / 2,
+                     coordinates_[1].second - size.height() / kScale / 2,
+                     size.width() / 2 / kScale,
+                     size.height() / kScale);
   // Draw first car
   PaintCar(painter,
-           width / 4 / kScale,
-           height / 2 / kScale,
+           size.width() / 4 / kScale,
+           size.height() / 2 / kScale,
            angles_[0]);
   // draw second car on first map
   PaintCar(painter,
            coordinates_[1].first - coordinates_[0].first
-               + width / 2 / kScale / 2,
+               + size.width() / 2 / kScale / 2,
            coordinates_[1].second - coordinates_[0].second
-               + height / kScale / 2,
+               + size.height() / kScale / 2,
            angles_[1]);
   // draw second car
   PaintCar(painter,
-           width / 4. * 3 / kScale,
-           height / 2. / kScale,
+           size.width() / 4. * 3 / kScale,
+           size.height() / 2. / kScale,
            angles_[1]);
   // draw first car on second map
-  painter->translate(width / 2 / kScale, 0);
+  painter->translate(size.width() / 2 / kScale, 0);
   PaintCar(painter,
            coordinates_[0].first - coordinates_[1].first
-               + width / 2 / kScale / 2,
+               + size.width() / 2 / kScale / 2,
            coordinates_[0].second - coordinates_[1].second
-               + height / kScale / 2,
+               + size.height() / kScale / 2,
            angles_[0]);
 }
 
