723fa480bf689d11a7c999749acd49ebd69989e1
Korbut-Anton <akorbut12@gmail.com>
2021-04-04 00:13:31 +0300

    Map data changes

--- src/helpers/map_data.h
+++ src/helpers/map_data.h
@@ -7,15 +7,22 @@
 namespace map_data {
 
 // First is data, second is image
-static const std::vector<std::pair<QString, QString>> map_filepaths{
-    {":resources/images/map_data/Map_1.txt",
-     ":resources/images/maps/Map_1.jpg"},
-    {":resources/images/map_data/Map_2.txt",
-     ":resources/images/maps/Map_2.jpg"},
-    {":resources/images/map_data/Map_3.txt",
-     ":resources/images/maps/Map_3.jpg"},
-    {":resources/images/map_data/Map_4.txt",
-     ":resources/images/maps/Map_4.jpg"}
+static const std::vector<QString> borders_filepaths{
+    {":resources/images/map_data/map_1.txt"},
+    {":resources/images/map_data/map_2.txt"},
+    {":resources/images/map_data/map_3.txt"},
+    {":resources/images/map_data/map_4.txt"},
+    {":resources/images/map_data/map_5.txt"},
+    {":resources/images/map_data/map_6.txt"}
+};
+
+static const std::vector<QString> image_filepaths{
+    {":resources/images/maps/map_1.jpg"},
+    {":resources/images/maps/map_2.jpg"},
+    {":resources/images/maps/map_3.jpg"},
+    {":resources/images/maps/map_4.jpg"},
+    {":resources/images/maps/map_5.jpg"},
+    {":resources/images/maps/map_6.jpg"}
 };
 
 }  // namespace map_data
