bdf16e70e7dfec778dd3acab6343d02b0d248c38
Korbut-Anton <akorbut12@gmail.com>
2021-04-03 22:36:53 +0300

    Some maps added Problems with their display

--- src/helpers/map_data.h
+++ src/helpers/map_data.h
@@ -8,12 +8,14 @@ namespace map_data {
 
 // First is data, second is image
 static const std::vector<std::pair<QString, QString>> map_filepaths{
-    {":resources/images/map_data/map_1.txt",
-     ":resources/images/maps/map_1.jpg"},
-    {":resources/images/map_data/map_2.txt",
-     ":resources/images/maps/map_2.jpg"},
-    {":resources/images/map_data/map_3.txt",
-     ":resources/images/maps/map_3.jpg"}
+    {":resources/images/map_data/Map_1.txt",
+     ":resources/images/maps/Map_1.jpg"},
+    {":resources/images/map_data/Map_2.txt",
+     ":resources/images/maps/Map_2.jpg"},
+    {":resources/images/map_data/Map_3.txt",
+     ":resources/images/maps/Map_3.jpg"},
+    {":resources/images/map_data/Map_4.txt",
+     ":resources/images/maps/Map_4.jpg"}
 };
 
 }  // namespace map_data
