b04afa04f07d0762de6e4276e3f882c60a52193b
Korbut-Anton <akorbut12@gmail.com>
2021-04-10 12:26:20 +0300

    More maps All chosen maps added. There are no more white or black borders on the edges of picture (in full screen mode also).

--- src/helpers/map_data.h
+++ src/helpers/map_data.h
@@ -8,21 +8,23 @@ namespace map_data {
 
 // First is data, second is image
 static const std::vector<QString> borders_filepaths{
-    {":resources/images/map_data/map_1.txt"},
-    {":resources/images/map_data/map_2.txt"},
-    {":resources/images/map_data/map_3.txt"},
-    {":resources/images/map_data/map_4.txt"},
-    {":resources/images/map_data/map_5.txt"},
-    {":resources/images/map_data/map_6.txt"}
+    {":resources/images/map_data/Map_1.txt"},
+    {":resources/images/map_data/Map_2.txt"},
+    {":resources/images/map_data/Map_3.txt"},
+    {":resources/images/map_data/Map_4.txt"},
+    {":resources/images/map_data/Map_5.txt"},
+    {":resources/images/map_data/Map_6.txt"},
+    {":resources/images/map_data/Map_7.txt"},
 };
 
 static const std::vector<QString> image_filepaths{
-    {":resources/images/maps/map_1.jpg"},
-    {":resources/images/maps/map_2.jpg"},
-    {":resources/images/maps/map_3.jpg"},
-    {":resources/images/maps/map_4.jpg"},
-    {":resources/images/maps/map_5.jpg"},
-    {":resources/images/maps/map_6.jpg"}
+    {":resources/images/maps/Map_1.jpg"},
+    {":resources/images/maps/Map_2.jpg"},
+    {":resources/images/maps/Map_3.jpg"},
+    {":resources/images/maps/Map_4.jpg"},
+    {":resources/images/maps/Map_5.jpg"},
+    {":resources/images/maps/Map_6.jpg"},
+    {":resources/images/maps/Map_7.jpg"}
 };
 
 }  // namespace map_data
