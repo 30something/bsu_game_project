32ecdb0baa2087330429732485a946825cf025b3
Fedor <fedorts2002@gmail.com>
2021-04-22 21:05:48 +0300

    Small fix

--- src/helpers/map_data.h
+++ src/helpers/map_data.h
@@ -23,12 +23,6 @@ class ImageFilePaths {
   ImageFilePaths();
 };
 
-static const std::vector<QString> image_filepaths{
-    {":resources/images/maps/map_1.jpg"},
-    {":resources/images/maps/map_2.jpg"},
-    {":resources/images/maps/map_3.jpg"}
-};
-
 static const ImageFilePaths image_file_paths;
 
 }  // namespace map_data
